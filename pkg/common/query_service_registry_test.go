5b19c3fefe87e161caa04abb16f3fd3f1b34f56c
psavelis <pedro@57blocks.com>
2026-01-05 09:57:20 -0300

    Clean up types and test imports
    - Remove unused TeamPRO constants from types.go
    - Fix import issues in test file

--- pkg/common/query_service_registry_test.go
+++ pkg/common/query_service_registry_test.go
@@ -1,23 +1,21 @@
 package shared
 
 import (
-"testing"
-
-shared "github.com/resource-ownership/go-common/pkg/common"
+	"testing"
 )
 
-// MockSchemaProvider implements shared.SchemaProvider for testing
+// MockSchemaProvider implements SchemaProvider for testing
 type MockSchemaProvider struct {
-	schema shared.QueryServiceSchema
+	schema QueryServiceSchema
 }
 
-func (m *MockSchemaProvider) GetQuerySchema() shared.QueryServiceSchema {
+func (m *MockSchemaProvider) GetQuerySchema() QueryServiceSchema {
 	return m.schema
 }
 
 func TestGetQueryServiceRegistry_Singleton(t *testing.T) {
-	registry1 := shared.GetQueryServiceRegistry()
-	registry2 := shared.GetQueryServiceRegistry()
+	registry1 := GetQueryServiceRegistry()
+	registry2 := GetQueryServiceRegistry()
 
 	if registry1 != registry2 {
 		t.Error("expected GetQueryServiceRegistry to return same instance")
--- pkg/common/types.go
+++ pkg/common/types.go
@@ -1,17 +1,5 @@
 package shared
 
-import "github.com/google/uuid"
-
-var (
-	// Default TenantID for TeamPRO (random and valid UUID)
-	TeamPROTenantID = uuid.MustParse("a3a80810-f91c-4391-9eff-6d47a13bebde")
-
-	// Default ClientID for TeamPRO (random and valid UUID)
-	TeamPROAppClientID = uuid.MustParse("ff96c01f-a741-4429-a0cd-2868d408c42f")
-
-	ServerClientID = uuid.MustParse("ff96c01f-a741-4429-a0cd-2868d408c42f")
-)
-
 type IntendedAudienceKey uint8
 
 type VisibilityTypeKey uint8
